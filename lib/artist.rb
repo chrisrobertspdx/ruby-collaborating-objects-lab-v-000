724d885302246b4313d88462af12a795511f2afa
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-30 17:04:08 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+
+
+class Artist
+  attr_accessor :name, :songs
+  @@artists = []
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  def add_song(song)
+    @songs << song
+  end
+  def self.find_or_create_by_name(name)
+    artist = @@artists.select{|e| e.name == name}
+    if artist.size > 0
+      artist[0]
+    else
+      Artist.new(name)
+    end
+  end
+  def self.all
+    @@artists
+  end
+  def save
+    @@artists << self
+  end
+  def print_songs
+    @songs.each{|e|
+      puts e.name
+    }
+  end
+end
